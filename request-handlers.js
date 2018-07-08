6aa7268d60169d9a63f7e128011f8e2a54ddaa0d
rbtprograms <rbt61690@gmail.com>
2018-07-07 18:58:29 -0700

    response object now passed to routers

--- request-handlers.js
+++ request-handlers.js
@@ -1,9 +1,20 @@
-function start() {
-    console.log("Request handler 'start' was called.")
+let exec = require('child_process').exec
+
+function start(res) {
+    console.log("Request handler 'start' was called.");
+
+    exec('ls -lah', function (error, stdout, stderr) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(stdout);
+        res.end();
+    });
 }
 
-function upload() {
+function upload(res) {
     console.log("Request handler 'upload' was called.")
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -6,11 +6,7 @@ function start(route, handle) {
         let pathname = url.parse(req.url).pathname;
         console.log('Request for ' + pathname + 'received.');
 
-        route(handle, pathname);
-
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write('Hello World');
-        res.end();
+        route(handle, pathname, res);
     }
 
     http.createServer(onRequest).listen(8888);
