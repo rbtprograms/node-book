c5054864104ca276408bd116633058ccbac037e5
rbtprograms <rbt61690@gmail.com>
2018-07-07 18:26:59 -0700

    router working

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 let http = require('http');
 let url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(req, res) {
         let pathname = url.parse(req.url).pathname;
-        console.log('Request for ' + pathname + 'received.')
+        console.log('Request for ' + pathname + 'received.');
+
+        route(pathname);
+
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.write('Hello World');
         res.end();
