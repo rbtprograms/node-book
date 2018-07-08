1203a66f93002394f3b3e88229a1074b3134f86b
rbtprograms <rbt61690@gmail.com>
2018-07-07 17:59:37 -0700

    some more server.js additions

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 let http = require('http');
 
 function onRequest(req, res) {
+    console.log('Request Received.')
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
 }
 
 http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
