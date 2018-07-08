4ece1efd703362bf7bdd55f67818db6df48c1d02
rbtprograms <rbt61690@gmail.com>
2018-07-07 17:59:52 -0700

    server now module

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 let http = require('http');
+function start() {
+    function onRequest(req, res) {
+        console.log('Request Received.')
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Hello World');
+        res.end();
+    }
 
-function onRequest(req, res) {
-    console.log('Request Received.')
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
+    http.createServer(onRequest).listen(8888);
+
+    console.log('Server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+exports.start = start;
