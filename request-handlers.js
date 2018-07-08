741397d29937db4e9a712a71a6fb37b7d1bbba88
rbtprograms <rbt61690@gmail.com>
2018-07-07 22:17:11 -0700

    posting working

--- request-handlers.js
+++ request-handlers.js
@@ -1,19 +1,31 @@
-let exec = require('child_process').exec
+const querystring = require('querystring');
 
 function start(res) {
     console.log("Request handler 'start' was called.");
 
-    exec('ls -lah', function (error, stdout, stderr) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(stdout);
-        res.end();
-    });
+    let body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(body);
+    res.end();
+
 }
 
 function upload(res) {
     console.log("Request handler 'upload' was called.")
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
+    res.write("You've sent the text :" + querystring.parse(postData).text);
     res.end();
 }
 
--- server.js
+++ server.js
@@ -3,14 +3,23 @@ let url = require('url');
 
 function start(route, handle) {
     function onRequest(req, res) {
+        let postData = "";
         let pathname = url.parse(req.url).pathname;
         console.log('Request for ' + pathname + 'received.');
 
-        route(handle, pathname, res);
+        req.setEncoding('utf8');
+
+        req.addListener('data', function(postDataChunk) {
+            postData += postDataChunk;
+            console.log('received POST data chunk ${postDataChunk}');
+        });
+
+        req.addListener('end', function() {
+            route(handle, pathname, res, postData);
+        });
     }
 
     http.createServer(onRequest).listen(8888);
-
     console.log('Server has started');
 }
 
